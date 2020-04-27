53558c33b275fe22d3283f1b583de2dbfb8d8ec4
도우진 <dwj1996@naver.com>
2020-04-27 22:47:26 +0900

    셋째 주 수업-1

---
+++ VRShooting/Assets/Scripts/PlayerGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour
+{
+
+    [SerializeField] GameObject bulltetPrefab;
+    [SerializeField] Transform gunBarrelEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) 
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulltetPrefab, gunBarrelEnd.position, gunBarrelEnd.rotation);
+    }
+}
