bbb56feb76bb1e3ec2b9bde468091ec1fd44df57
도우진 <dwj1996@naver.com>
2020-04-27 23:23:57 +0900

    셋째 주 수업-2

---
+++ VRShooting/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 20f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var velocity = speed * transform.forward;
+
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        other.SendMessage("OnHitBullet");
+        Destroy(gameObject);
+    }
+}
