12e503de68332fd1b735baf4030e82f87ad6ae9a
도우진 <dwj1996@naver.com>
2020-04-28 00:14:12 +0900

    넷째 주 수업-1

---
+++ VRShooting/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] Enemy enemyPrefab;
+
+    Enemy enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        if(enemy==null)
+         {
+             enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+         }
+    }
+}
--- VRShooting/Assets/Scripts/Shooter.cs
+++ VRShooting/Assets/Scripts/Shooter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerGun : MonoBehaviour
+public class Shooter : MonoBehaviour
 {
 
     [SerializeField] GameObject bulltetPrefab;
